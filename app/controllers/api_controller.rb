e0faeae3206a51c3f01035799ba51e35ffbab59b
nrahnemoon <nrahnemoon@gmail.com>
2016-11-29 01:25:09 -0500

    Another attempt.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -86,6 +86,7 @@ class ApiController < ApplicationController
 	# Params: robotID, lat, lng, batteryLevel, signalStrength, binFullness
 	def set_heartbeat
 
+		puts request.raw_post
 		params = JSON.parse(request.raw_post)
 
 		robot = Robot.find_by(:id => params[:robotID])
